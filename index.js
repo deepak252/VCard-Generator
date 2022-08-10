ec0e978b0d9d8db1fc1180528d047ca4250cc2b9
Deepak <trojanhorsedc@gmail.com>
2022-08-11 00:40:05 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require("cors");
+
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
 app.use(bodyParser.json());
 const PORT = process.env.PORT || '5000';
 
